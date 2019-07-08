03ffb8f4ff8a901bba3191c4d793396d6be571ad
spinon187 <spinon187@gmail.com>
2019-07-08 14:17:05 -0500

    added redux boilerplate too which I forgot

--- src/index.js
+++ src/index.js
@@ -2,6 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {applyMiddleware, createStore} from 'redux';
+import rootReducer from './reducers/index';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import {loadState, saveState} from './components/LocalStorage';
+
+const persistedStore = loadState();
+const store = createStore(
+    rootReducer,
+    persistedStore,
+    applyMiddleware(thunk, logger),
+    
+  );
+
+store.subscribe(() => {
+    saveState(store.getState())
+  })
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+        <Route path='/' component={App} />
+    </Router>
+  </Provider>,  
+  document.getElementById('root')
+);
