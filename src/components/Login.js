c7ecadf5f3cf54e5a8b230a3ec2671856b4f5e7b
spinon187 <spinon187@gmail.com>
2019-07-08 18:23:22 -0500

    hooked up login component to redux

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,90 +1,62 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-//import DashBoard from './dashboard';
-import './App.css';
+import { connect } from 'react-redux';
+import {login} from '../actions';
 
 
-const apiBaseUrl = "";
 
 class Login extends Component {
     constructor(props){
         super(props);
         this.state={
-        username:'',
-        password:'',
-        usertype: 0
+          username:'',
+          password:'',
         }
-       }
-      render() {
-          return (
-            <div>
+    }
+
+    login = e => {
+      e.preventDefault();
+      this.props.login(this.state);
+      this.props.history.push('/');
+    }
+    
+    render() {
+        return (
+          <div>
+            
+              <div title="Login">
+                <form onSubmit={this.login}>
+                  <input
+                  placeholder="Enter your username"
+                  //value={this.state.username}
+                  name='username'
+                  type='text'
+                  onChange = {(e,newValue) => this.setState({username:newValue})}
+                  />
+                <br/>
+                    <input
+                    placeholder='Enter your password'
+                  // value={this.state.password}
+                    name='password'
+                    type='password'
+                    onChange = {(e,newValue) => this.setState({password:newValue})}
+                    />
+                  <br/>
+                  <button className='login-button' type='submit'>Log In</button>
+                  </form>
+              </div>
               
-                <div title="Login">
-                 <form>
-                   <input
-                   placeholder="Enter your username"
-                   //value={this.state.username}
-                   name='username'
-                   type='text'
-                   onChange = {(e,newValue) => this.setState({username:newValue})}
-                   />
-                 <br/>
-                     <input
-                     placeholder='Enter your password'
-                    // value={this.state.password}
-                     name='password'
-                     type='password'
-                     onChange = {(e,newValue) => this.setState({password:newValue})}
-                     />
-                   <br/>
-                   <button className='login-button' onClick={(e) => this.handleClick(e)}>Log In</button>
-                   </form>
-               </div>
-               
-            </div>
-          );
-        }
+          </div>
+        );
       }
-    // this.state={
-    //   email:'',
-    //   password:'',
-    //   menuValue:1,
-    //   //loginComponent:localloginComponent,
-    //   loginRole:'name'
-    // }
-  
-  function handleClick(e){
-    const apiBaseUrl = "";
-    const self = this;
-    const payload={
-    "username":this.state.username,
-    "password":this.state.password
     }
-    axios.post(apiBaseUrl+'login', payload)
-    .then(function (response) {
-    console.log(response);
-      if(response.data.code === 200){
-      console.log("Login successful");
-      this.props.history.push('/dashboard')
-       const dashboard=[];
-       dashboard.push(<DashBoard appContext={self.props.appContext}/>)
-       self.props.appContext.setState({loginPage:[],dashboard:dashboard})
-      }
-      else if(response.data.code === 204){
-      console.log("username and password do not match");
-      alert("username and password do not match")
-      }
-      else{
-      console.log("username does not exists");
-      alert("username does not exist");
-      }
-    })
-    .catch(function (error) {
-    console.log(error);
-    });
-    }
+
   
 
+const mapStateToProps = state => {
+    return {
+        isLoading: state.login.isLoading,
+        error: state.login.error
+    }
+}
 
-
-export default Login;
+export default connect(mapStateToProps, {login})(Login);
--- src/views/index.js
+++ src/views/index.js
@@ -1,1 +1,3 @@
 export {default as SignupView} from './SignupView';
+export {default as LoginView} from './LoginView';
+export {default as MainView} from './MainView';
