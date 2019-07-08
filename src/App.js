ea624df45760ec1d5f0b72bd1bc45ee3d3bd5b27
droneal11015 <droneal11015@gmail.com>
2019-07-08 18:15:42 -0400

    built out app.js page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+//import Loginscreen from './loginscreen';
+//import DashBoard from './dashboard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <p>hi</p>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      loginPage:[],
+      //uploadScreen:[]
+    }
+  }
+  componentWillMount(){
+    var loginPage =[];
+    loginPage.push(<Loginscreen parentContext={this}/>);
+    this.setState({
+                  loginPage:loginPage
+                    })
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.loginPage}
+        {this.state.dashboard}
+      </div>
+    );
+  }
 }
+// if(response.data.code == 200){
+//   console.log("Login successfull");
+//   const dashboard=[];
+//   dashboard.push(<DashBoard appContext={self.props.appContext}/>)
+//   self.props.appContext.setState({loginPage:[],dashboard:dashboard})
+//   }
+
+
 
 export default App;
