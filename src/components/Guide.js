bfc74e15aac3fabd8fed221b70fe75285331bdb7
spinon187 <spinon187@gmail.com>
2019-07-09 11:13:13 -0500

    basic individual guide component

---
+++ src/components/Guide.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+class Message extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      title: this.props.guidename,
+      owner: this.props.owner,
+      content: this.props.guidecontent,
+      date: this.props.dateposted,
+      category: this.props.category,
+
+    }
+  }
+
+  render(){
+    return(
+      <>
+        <h2>{this.state.title}</h2>
+        <h3>by {this.state.owner}</h3>
+        <div>
+          {this.state.content}
+        </div>
+        <h4>Posted {this.state.date}</h4>
+        <h2>Filed Under: {this.state.category}</h2>
+      </>
+    )
+  }
+}
+
+export default Message;
