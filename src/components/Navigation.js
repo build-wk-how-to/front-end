70f6d3931c9ccf5130f36bb4965bb5f178d7278a
spinon187 <spinon187@gmail.com>
2019-07-09 10:42:42 -0500

    added .inUse to css file

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,8 +4,8 @@ import { NavLink, Link} from 'react-router-dom';
 export const Navigation = props => {
 
   const navSwap = props.loggedIn === false 
-      ? <Link to='/'>Log in</Link>
-      : <><Link to='/'>Home</Link> <h2 onClick={props.logout}>log out</h2></>
+      ? <Link to='/'>Log In</Link>
+      : <><Link to='/'>Home</Link> <h2 onClick={props.logout}>Log Out</h2></>
 
   const signSwap = props.loggedIn === false
       && <NavLink to='/signup' activeClassName='inUse'>Sign Up</NavLink>
