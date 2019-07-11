ecfba40f7d4e779ffbdb262a00bc632ccfaa7e1f
droneal11015 <droneal11015@gmail.com>
2019-07-11 15:23:53 -0400

    matched login.js to master file to resolve merge conflicts

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,62 +7,70 @@ import './Components.css';
 
 
 class Login extends Component {
-    constructor(props){
-        super(props);
-        this.state={
-          username:'',
-          password:'',
-        }
-    }
+  constructor(props){
+      super(props);
+      this.state={
+        username:'',
+        password:'',
+      }
+  }
 
-    login = e => {
-      e.preventDefault();
-      this.props.login(this.state);
-      this.props.history.push('/');
-    }
+  login = e => {
+    e.preventDefault();
+    this.props.login(this.state);
+    this.props.history.push('/');
+  }
 
-    
-    render() {
-        return (
+  input = e => {
+    this.setState({
+      ...this.state,
+      [e.target.name]: e.target.value 
+    });
+  };
+  
+  render() {
+      return (
+        
+        <div className="login-page">
           
-          <div className="login-page">
-
-              <div title="Login" className="login-form">
-                <form onSubmit={this.login}>
-                  <h4>Username:</h4>
+            <div title="Login" className="login-form">
+              <form onSubmit={this.login}>
+                <h4>Username:</h4>
+                <input
+                placeholder="Enter your username"
+                //value={this.state.username}
+                name='username'
+                value={this.state.username}
+                type='text'
+                onChange = {this.input}
+                />
+              <br/>
+                <h4>Password:</h4>
                   <input
-                  placeholder="Enter your username"
-                  //value={this.state.username}
-                  name='username'
-                  type='text'
-                  onChange = {(e,newValue) => this.setState({username:newValue})}
+                  placeholder='Enter your password'
+                // value={this.state.password}
+                  name='password'
+                  value={this.state.password}
+                  type='password'
+                  onChange = {this.input}
                   />
                 <br/>
-                  <h4>Password:</h4>
-                    <input
-                    placeholder='Enter your password'
-                  // value={this.state.password}
-                    name='password'
-                    type='password'
-                    onChange = {(e,newValue) => this.setState({password:newValue})}
-                    />
-                  <br/>
-                  <button className='login-button' type='submit'>Log In</button>
-                  </form>
-              </div>
-              
-          </div>
-        );
-      }
+                <button className='login-button' type='submit'>Log In</button>
+                </form>
+            </div>
+            
+        </div>
+      );
     }
+  }
 
-  
+
 
 const mapStateToProps = state => {
-    return {
-        isLoading: state.login.isLoading,
-        error: state.login.error
-    }
+  return {
+      isLoading: state.login.isLoading,
+      error: state.login.error
+  }
 }
 
 export default connect(mapStateToProps, {login})(Login);
