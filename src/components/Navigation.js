8bdb1011ecb1126889810fc4405a3c1709a6951f
Greyflanel <richardlovelace811@gmail.com>
2019-07-12 03:26:36 -0500

    my first commit. I did a little styling so the sign up and log in were on top of form.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import { NavLink, Link} from 'react-router-dom';
-import './Components.css';
-import logo from './how-to-logo.jpg.png'
-
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import "./Components.css";
+import logo from "./how-to-logo.jpg.png";
 
 export const Navigation = props => {
+  const navSwap =
+    props.loggedIn === false ? (
+      <Link to="/">Log In</Link>
+    ) : (
+      <>
+        <Link to="/">Home</Link> <h2 onClick={props.logout}>Log Out</h2>
+      </>
+    );
 
-  const navSwap = props.loggedIn === false 
-      ? <Link to='/'>Log In</Link>
-      : <><Link to='/'>Home</Link> <h2 onClick={props.logout}>Log Out</h2></>
+  const signSwap = props.loggedIn === false && (
+    <NavLink to="/signup" activeClassName="inUse">
+      Sign Up
+    </NavLink>
+  );
 
-  const signSwap = props.loggedIn === false
-      && <NavLink to='/signup' activeClassName='inUse'>Sign Up</NavLink>
-  
-  return(
+  return (
+    <div>
+      <img id="logo" src={logo} alt="" />
+      <h1>How-To</h1>
       <div className="navigation-page">
-          <img id ="logo" src={logo}/>
-          <h1>How-To</h1>
-          <div className="navigation-links">
-              {navSwap}              
-              {signSwap}
-          </div>
+        <div />
       </div>
-  )
-}
+      <div className="nav">
+        {navSwap}
+        {signSwap}
+      </div>
+    </div>
+  );
+};
